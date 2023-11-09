fa320c1fdd70d16f8dbcc76d041de1c78969496a
Saisana299 <yt0521kariaka@gmail.com>
2023-11-09 19:51:48 +0900

    [Edit]: ToDo - Implement avoidance processing and turning processing.

--- src/main.py
+++ src/main.py
@@ -2,14 +2,20 @@ import cv2
 import numpy as np
 import time
 import ttc
-import sys
 import arrow
 import command
 
 prev_time = time.time()
 prev_time2 = time.time()
+
 frame_buffer = []
+
 close_count = 0
+is_avoid = False
+
+arrow_count = 0
+old_direction = ""
+is_turn = False
 
 # TCPクライアントのセットアップ
 command = command.Command()
@@ -26,8 +32,7 @@ ttc = ttc.TTC()
 # TTCの警告閾値
 TTC_THRESHOLD = 0.1 # 比較的厳しめ
 
-def show_arrow_info(frame, direction, relative, lr, dx, ud, dy):
-    text = f"Arrow:{direction}, X:{dx}({lr}), Y:{dy}({ud}), Z:{relative}"
+def show_arrow_info(frame, text):
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1
     thickness = 2
@@ -67,25 +72,6 @@ while True:
 
         prev_time = current_time
 
-    if elapsed_time2 >= 0.25: # 0.25秒ごとに処理
-        
-        # 矢印判定とカメラ映像取得
-        new_frame, direction, relative, position = arrow.Arrow().analysis(frame)
-        if((direction is not None) & (relative is not None) & (position is not None)):
-            #todoそれぞれでnone判定したほうがいいかも
-            lr, ud, dx, dy = position
-
-            show_arrow_info(new_frame, direction, relative, lr, dx, ud, dy)
-
-        #todo 同方向検知5回程度で操作実行？→実行中は判定結果加算しない
-        #     距離を測定して前方後方位置調整
-
-        # 出力
-        cv2.namedWindow('Tello-Detection', cv2.WINDOW_NORMAL)
-        cv2.imshow("Tello-Detection", new_frame)
-
-        prev_time2 = current_time
-
     # TTCの平均を計算して警告を出す
     if len(frame_buffer) == 5:
         average_ttc = np.mean(frame_buffer)
@@ -94,14 +80,55 @@ while True:
         # 至近距離判定カウント
         if average_ttc < TTC_THRESHOLD:
             print("TTC: Object is very close.")
-            # 数回でstopコマンド送信→実行後は旋回＆一定時間判定結果加算しない
             close_count += 1
-            #if(close_count >= 5):
-                #command.send("command") # todo
+            if((not is_avoid) & (not is_turn)):
+                # 5連続以上で接近判定になったら停止させ回避行動を開始
+                if(close_count >= 5):
+                    is_avoid = True
+                    command.send("ABCDEFGHIJKLNMOPQRSTUVWXYZ1234567890") # todo
         else:
             close_count = 0
 
+        if is_avoid:
+            # 一定時間(回避処理終了)後再検査 (okのレスポンスでも良い？)
+            _ = "todo"
+
         frame_buffer = []
+
+    # 矢印判定処理
+    if elapsed_time2 >= 0.25: # 0.25秒ごとに処理
+    
+        # 矢印判定とカメラ映像取得
+        new_frame, direction, relative, position = arrow.Arrow().analysis(frame)
+        if((direction is not None) & (relative is not None) & (position is not None)):
+            lr, ud, dx, dy = position
+            show_arrow_info(new_frame, f"Arrow:{direction}, X:{dx}({lr}), Y:{dy}({ud}), Z:{relative}")
+
+            # 一定の距離以内に近づかないと数えない
+            if(relative >= 100000):
+                if(old_direction == direction): arrow_count += 1
+                old_direction = direction
+
+            # 5回連続で方向検知で方向転換
+            if((not is_avoid) & (not is_turn)):
+                if(arrow_count >= 5):
+                    is_turn = True
+                    command.send("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ") # todo
+
+            elif is_turn:
+                # 位置調整など 指示ごとに一定時間経過後処理 (okレスポンスでも良い？)
+                _ = "todo"
+
+        else:
+            arrow_count = 0
+            old_direction = ""
+
+        # 出力
+        cv2.namedWindow('Tello-Detection', cv2.WINDOW_NORMAL)
+        cv2.imshow("Tello-Detection", new_frame)
+
+        prev_time2 = current_time
+    
     
     # 'Esc'キーで終了
     if cv2.waitKey(1) & 0xFF == 27:
